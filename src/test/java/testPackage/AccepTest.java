9e6a751c6c875af45452172ff2170a7428641319
Sundosramadan <sundos.m.ramadan12@gmail.com>
2025-05-02 20:14:28 +0300

    hi

--- src/test/java/testPackage/AccepTest.java
+++ src/test/java/testPackage/AccepTest.java
@@ -1,20 +1,17 @@
 package testPackage;
-import org.junit.runner.RunWith;
 
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-		features = "src/feature",
-	    plugin = { "pretty", "html:target/cucumber/wikipedia.html" }, 
-	    monochrome = true, 
-	    snippets = SnippetType.CAMELCASE, 
-	    glue = "fit"
-	)
-
+    features = "src/test/resources",   // مكان ملفات .feature
+    glue = "testPackage",              // مكان step definitions
+    plugin = { "pretty", "html:target/cucumber-report.html" },
+    monochrome = true,
+    snippets = SnippetType.CAMELCASE
+)
 public class AccepTest {
-
 }
