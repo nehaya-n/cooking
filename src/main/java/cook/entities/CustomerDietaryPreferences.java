ff7b79d74e698b1008ab0b33c34c82ca3fcb7fcc
nehaya-n <s12113008@stu.najah.edu>
2025-05-16 20:11:01 +0300

    Update CustomerDietaryPreferences.java

--- src/main/java/cook/entities/CustomerDietaryPreferences.java
+++ src/main/java/cook/entities/CustomerDietaryPreferences.java
@@ -13,43 +13,17 @@ public class CustomerDietaryPreferences {
         this.customerName = customerName;
         this.dietaryRestrictions = new HashSet<>();
     }
+    
 
     // Add a dietary restriction to the customer
     public void addDietaryRestriction(String restriction) {
+
         dietaryRestrictions.add(restriction);
     }
-
-  /*  // Remove a dietary restriction from the customer
-    public void removeDietaryRestriction(String restriction) {
-        dietaryRestrictions.remove(restriction);
-    }
-
-    // Get all dietary restrictions for this customer
+    
     public Set<String> getDietaryRestrictions() {
         return new HashSet<>(dietaryRestrictions);
     }
 
-    // Check if the customer has a specific dietary restriction
-    public boolean hasDietaryRestriction(String restriction) {
-        return dietaryRestrictions.contains(restriction);
-    }
-
-    // Get customer name
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    // Set customer name
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerDietaryPreferences{" +
-                "customerName='" + customerName + '\'' +
-                ", dietaryRestrictions=" + dietaryRestrictions +
-                '}';
-    }*/
-    
+  
 }
